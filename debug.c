a9e75074ab0dd13d88f7a73464cb768030750e0e
Suraj Pulloor <thegamer600@gmail.com>
2021-10-03 23:35:23 +0530

    Added local vars to clox

--- debug.c
+++ debug.c
@@ -80,6 +80,12 @@ int disassembleInstruction(Chunk* chunk, int offset)
         case OP_SET_GLOBAL:
             return constantInstruction("OP_SET_GLOBAL", chunk, offset);
 
+        case OP_GET_LOCAL:
+            return byteInstruction("OP_GET_LOCAL", chunk, offset);
+
+        case OP_SET_LOCAL:
+            return byteInstruction("OP_SET_LOCAL", chunk, offset);
+
     default:
         printf("Unknown opcode %d\n", instruction);
         return offset + 1;
@@ -101,4 +107,12 @@ static int simpleInstruction(const char* name, int offset)
 {
     printf("%s\n", name);
     return offset + 1;
+}
+
+
+static int byteInstruction(const char* name, Chunk* chunk, int offset)
+{
+    uint8_t slot = chunk->code[offset + 1];
+    printf("%-16s %4d\n", name, slot);
+    return offset + 2;
 }
