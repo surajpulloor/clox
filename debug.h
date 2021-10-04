7d2aef4a5b297600cd0408157f7c78ed485f74d0
Suraj Pulloor <thegamer600@gmail.com>
2021-10-05 00:25:09 +0530

    Added control flow except for loop.

--- debug.h
+++ debug.h
@@ -8,5 +8,6 @@ int disassembleInstruction(Chunk* chunk, int offset);
 static int simpleInstruction(const char* name, int offset);
 static int constantInstruction(const char* name, Chunk* chunk, int offset);
 static int byteInstruction(const char* name, Chunk* chunk, int offset);
+static int jumpInstruction(const char* name, int sign, Chunk* chunk, int offset);
 
 #endif // clox_debug_h
